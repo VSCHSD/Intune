ff0eaf592c0eeea185f09b787d7a8117e8ad02a4
VSCHSD <support@vschsd.org>
2026-05-11 10:19:11 -0400

    Add files via upload

--- OSDCloud/Start-OSDCloud.ps1
+++ OSDCloud/Start-OSDCloud.ps1
@@ -1,5 +1,3 @@
-#Start-OSDCloud -Firmware -Restart -ZTI -OSName 'Windows 11 25H2 x64' -OSLanguage en-us -OSEdition Pro -OSActivation Retail
-
 #Variables to define the Windows OS / Edition etc to be applied during OSDCloud
 $OSName = 'Windows 11 25H2 x64'
 $OSEdition = 'Pro'
@@ -28,8 +26,8 @@ $Global:MyOSDCloud = [ordered]@{
     updateFirmware = [bool]$true
     updateNetworkDrivers = [bool]$true
     updateSCSIDrivers = [bool]$true
-    OSNames = @('Windows 11 25H2 x64', 'Windows 11 24H2 x64')
-    OSVersionNames = @('Windows 11')
+    #OSNames = @('Windows 11 25H2 x64', 'Windows 11 24H2 x64')
+    #OSVersionNames = @('Windows 11')
     BrandName = 'Valley Stream Central High School District'
 }
 
@@ -43,6 +41,41 @@ if ($Manufacturer -match 'HP|Hewlett') {
     Write-Host "Manufacturer is $Manufacturer, setting BIOS settings" -ForegroundColor Green
     Invoke-WebPSScript 'https://raw.githubusercontent.com/VSCHSD/Intune/refs/heads/main/OSDCloud/Manage-HPBiosSettings.ps1'
 }
-#Launch OSDCloud
-Write-Host "Starting OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage" -ForegroundColor Green
-Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+
+do {
+
+    Write-Host ""
+    Write-Host "===== OSDCloudCLI Configuration =====" -ForegroundColor Cyan
+    Write-Host "OS Name       : $OSName"
+    Write-Host "OS Edition    : $OSEdition"
+    Write-Host "Activation    : $OSActivation"
+    Write-Host "Language      : $OSLanguage"
+    Write-Host "==================================" -ForegroundColor Cyan
+    Write-Host ""
+
+    Write-Host "Select OSDCloud startup mode:" -ForegroundColor Cyan
+    Write-Host "1. Start OSDCloudCLI Unattended (With the options above)"
+    Write-Host "2. Start OSDCloudGUI (To pick others)"
+    Write-Host ""
+
+
+    $Choice = Read-Host "Enter selection (1 or 2)"
+
+    if ($Choice -notin '1','2') {
+        Write-Host "Invalid selection. Please enter 1 or 2." -ForegroundColor Yellow
+    }
+
+} until ($Choice -in '1','2')
+
+switch ($Choice) {
+    '1' {
+        #Launch OSDCloud
+        Write-Host "Starting OSDCloudCLI -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage" -ForegroundColor Green
+        Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+    }
+    '2' {
+        Write-Host "Starting OSDCloudGUI..." -ForegroundColor Green
+        Start-OSDCloudGUI
+    }
+}
